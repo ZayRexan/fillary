58c948d9b815a8aadb22e5a0fcf951721534cc97
Konstantin <mogilevkostya@yandex.ru>
2025-08-20 08:59:06 +0700

    Модалка: вёрстка и адаптация

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-empty */
 import { initHeader } from './modules/header/header.js';
 import { initSwiper } from './scripts/initSwiper.js';
 import { initDrag } from './scripts/initDrag.js';
+import { initModal } from './scripts/initModal.js';
 
 initHeader();
 initSwiper();
-initDrag();
+try {
+	initDrag();
+} catch {}
+initModal();
